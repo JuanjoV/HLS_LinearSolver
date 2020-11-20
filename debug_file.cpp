6b94585612e96e201a5c64d3dd6be7b8d43bbe4b
Juan <juan.vasquezca@sansano.usm.cl>
2020-11-20 00:34:03 -0300

    Tcl file completed. Readme notes added

--- debug_file.cpp
+++
@@ -1,4 +0,0 @@
-#include <fstream>
-#include <string>
-#include <sstream>
-
--- run_hls.tcl
+++ run_hls.tcl
@@ -1,28 +1,38 @@
-############################################################
-## This file is generated automatically by Vivado HLS.
-## Please DO NOT edit it.
-## Copyright (C) 2013 Xilinx Inc. All rights reserved.
-############################################################
+######################################
+# You can change the initialization paramters below
+# Inspired on automatically generated file by Vivado HLS.
+######################################
 
 # Create a Vivado HLS project
 open_project -reset CholeskyLinearSolver_prj
 set_top CholeskyLS
 add_files CholeskyLS.cpp
+add_files -tb out.gold.dat
+add_files -tb CholeskyLS_test.cpp
+
 
 # Solution1 *************************
 open_solution -reset "solution1"
-#set_part  {xcvu9p-flgb2104-1-e}
+
 #zybo part
 set_part {xc7z010-clg400-1}
+#Objetive clock
 create_clock -period 10
 
-# Run C simulation
-#csim_design
-# Run Synthesis
+
+## Uncomment the following lines with a single # if your code is already 
+## verified
+
+## Run C simulation
+#sim_design
+
+## Run Synthesis
 #csynth_design
-# Run RTL verification
+
+## Run RTL verification
 #cosim_design
-# Create the IP package
+
+## Create the IP package
 #export_design
 
 exit
